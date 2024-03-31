9fff3b8e6f7e1e874a77db492671916d57d519f8
JouniGitti <kalaksi@gmail.com>
2024-03-31 18:23:29 +0300

    Stopped TDD development - numerous errors possibly due to the incompatible libraries

--- tuf-ui-app/__tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../src/app/page.js';
-
-describe('Index', () => {
-    it('renders correctly', () => {
-        render(<Home />);
-        expect(screen.getByText('Hello World')).toBeInTheDocument();
-    });
-});
