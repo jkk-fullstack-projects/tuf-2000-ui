b48a3bfa410d02b960ce055cb7b2db3e14fa383b
JouniGitti <kalaksi@gmail.com>
2024-04-01 09:47:11 +0300

    Changes to sidebar so that data shows in console.log - also one data element shown in browser. Added mocked meter data in json format.

--- tuf-ui-app/src/app/components/Sidebar/Sidebar.js
+++ tuf-ui-app/src/app/components/Sidebar/Sidebar.js
@@ -1,26 +1,39 @@
-"use client";
+"use client"; 
+// This is a client-side component, 
+// this is required by the next.js framework 
+// to render the component on the client-side
 
-import React from "react";
+import React, { useState } from "react";
 import dynamic from 'next/dynamic';
 const DateRangePicker = dynamic(() => import('react-date-range')
     .then((mod) => mod.DateRangePicker), {ssr: false,});
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
+import mockData from '../../../../mock_data/Mocked_meter_data.json';
+
+console.log('Mocked Data import:', mockData)
 
 const Sidebar = () => {
-    const [state, setState] = React.useState([
+    const [state, setState] = useState([
         {
           startDate: new Date(),
           endDate: new Date(),
           key: 'selection',
         },
     ]);
+    const [fetchedData, setFetchedData] = useState(null);
 
     const handleFetchData = () => {
-        // Fetch data from the API
+        // Simulate fetch data from the API
         console.log('Mocking Fetch data from the API');
         console.log(state[0].startDate, state[0].endDate);
         // a function call will be added to fetch data based on selected dates
+        setTimeout(() => {
+            // Directly use the mock data
+            console.log('Fetched Data:', mockData)
+            setFetchedData(mockData);
+            // now the data can be passed to another component
+          }, 1000); // Simulate network delay
     };
     
 
@@ -34,7 +47,9 @@ const Sidebar = () => {
                 onChange={(item) => setState([item.selection])}
             />
         </div>
-        <button onClick={handleFetchData}>Get Meter Data</button>
+        <div className="fetch-data-button">
+            <button onClick={handleFetchData}>Get Meter Data</button>
+        </div>
         {/* Toggle Button */}
         <div className="toggle-button">
             {/* Toggle button component will be here */}
@@ -44,6 +59,17 @@ const Sidebar = () => {
         <div className="timestamps">
             {/* Timestamps list will be rendered here */}
         </div>
+        {fetchedData && (
+        <div className="fetched-data">
+            <h5>Fetched Data:</h5>
+        {/* fetched data rendered here, adding a component later */}
+            {fetchedData.data.map((item, index) => (
+                <div key={index}>
+                    {item.variableName}: {item.value} {item.unit}
+                </div>
+            ))}
+        </div>
+        )}
     </div>
     );
 };
